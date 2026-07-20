5d369ecc4c182e2d00f6405b81766f8880bf958e
arnold-O <arnoldoghiator@gmail.com>
2026-07-20 23:10:56 +0100

    shell strict intro

---
+++ shell-scripting/shell.sh
@@ -0,0 +1,3 @@
+# Shell scripting is seet of executatable files that execute sequencially
+# it start with a #!/bin/bash to denote with shell this  script should be executed in
+# A script should have executable permission as it is just another file e.g  -rwxr-x--x
